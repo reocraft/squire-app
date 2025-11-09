7fe15e843754f9d0363ffec08f6fd4d39f9d070a
matt <malekmat@grinnell.edu>
2025-11-09 06:09:44 -0500

    made text green

--- client/app/page.js
+++ client/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
     <div className="flex relative min-h-screen items-center justify-center font-sans dark:bg-white">
       <main className="absolute inset-1 flex flex-col space-y-4 z-1">
         <div className="px-10 justify-start">
-          <h1 className="h-full w-full flex items-center justify-left text-[clamp(2em,2vw,3vw)] font-bold text-[clamp(2em,2vw,3vw)]">Healthy nutrition, made simple</h1>
+          <h1 className="h-full w-full flex items-center text-green-400 justify-left text-[clamp(2em,2vw,3vw)] font-bold text-[clamp(2em,2vw,3vw)]">Healthy nutrition, made simple</h1>
         </div>
         <div className="flex justify-center">
          <section id="signup" className="text-center py-20 bg-indigo-600 text-white px-20 justify-center ">
